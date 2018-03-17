b65e5e044638a0ef017167425edddbcdc787e70d
pazpazon <35139232+pazpazon@users.noreply.github.com>
2018-03-17 12:20:24 +0100

    added geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -15,6 +15,17 @@ socket.on('newMessage', function(message){
   jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(locationMessage){
+  console.log('newLocationMessage received', locationMessage);
+  var li =  jQuery('<li></li>');
+  var link = jQuery('<a target="_blank">Location</a>');
+
+  li.text(`${locationMessage.from}: `);
+  link.attr('href', locationMessage.url);
+  li.append(link);
+  jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function(e){
   e.preventDefault();
 
@@ -23,3 +34,21 @@ jQuery('#message-form').on('submit', function(e){
     text: jQuery('[name=message-text]').val()
   }, function(){});
 });
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function (e){
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+  navigator.geolocation.getCurrentPosition(function(position){
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function(err){
+    alert('Unable to fetch location...', err);
+  });
+});
+
+
+
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generteMessage', () => {
   it('should generate correct message object', () => {
@@ -8,5 +8,16 @@ describe('generteMessage', () => {
     expect(typeof message).toBe('object');
     expect(message).toMatchObject(message);
     expect(typeof message.createdAt).toBe('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location message object', () => {
+    let lat = 123231, lon = 45654;
+    let location = generateLocationMessage('Admin', lat, lon);
+    expect(typeof location).toBe('object');
+    expect(location.from).toBe('Admin');
+    expect(typeof location.createdAt).toBe('number');
+    expect(location.url).toBe(`https://www.google.com/maps/?q=${lat},${lon}`);
   });
 });
